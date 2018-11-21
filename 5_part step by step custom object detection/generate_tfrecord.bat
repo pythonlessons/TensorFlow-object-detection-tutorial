23ab5eb1f44975726dabe98f7dcabd54757e8e54
pythonlessons <43852392+pythonlessons@users.noreply.github.com>
2018-11-21 16:19:46 +0200

    Add files via upload

---
+++ 5_part step by step custom object detection/generate_tfrecord.bat
@@ -0,0 +1,2 @@
+python generate_tfrecord.py --csv_input=CSGO_images\train_labels.csv --image_dir=CSGO_images\train --output_path=CSGO_images\train.record
+python generate_tfrecord.py --csv_input=CSGO_images\test_labels.csv --image_dir=CSGO_images\test --output_path=CSGO_images\test.record
---
+++ 5_part step by step custom object detection/training.bat
@@ -0,0 +1,1 @@
+python train.py --logtostderr --train_dir=CSGO_training_dir/ --pipeline_config_path=CSGO_training/faster_rcnn_inception_v2_coco.config
